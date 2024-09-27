3800986626b9d3a28c407dd4a713a09ed6f07695
Ali Abdul Wahid <aly.dihaw96@gmail.com>
2024-09-27 16:36:20 +0700

    Remove Secrets

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,4 @@
 export const environment = {
-    firebase: {
-      apiKey: "AIzaSyBEMx3JBAZXk9HmpNv2bt24VLrv6sfl518",
-      authDomain: "portalnews-86a4c.firebaseapp.com",
-      projectId: "portalnews-86a4c",
-      storageBucket: "portalnews-86a4c.appspot.com",
-      messagingSenderId: "972523020072",
-      appId: "1:972523020072:web:61848c7917b0b23b583a1d"
-    },
     production: false,
     env: 'prod',
     key: 'portalnews',
